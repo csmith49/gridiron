37bb704a5f42a62efbf0b75c3f1031cadc5bfc6a
Calvin Smith <email@cjsmith.io>
2021-12-22 13:24:28 -0600

    initial

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+const now = String(Date.now())
+const htmlmin = require('html-minifier')
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
+    eleventyConfig.addWatchTarget('./styles/tailwind.css')
+    eleventyConfig.addPassthroughCopy({'./_tmp/style.css' : './style.css'})
+    
+    eleventyConfig.addShortcode('version', function () {return now})
+
+    eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+        if (
+            process.env.ELEVENTY_PRODUCTION &&
+            outputPath &&
+            outputPath.endsWith('.html')
+        ) {
+            let minified = htmlmin.minify(
+                content, {
+                    useShortDoctype : true,
+                    removeComments : true,
+                    collapseWhitespace : true,
+                }
+            );
+            return minified
+        }
+        return content
+    }
+}
